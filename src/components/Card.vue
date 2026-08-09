d50016ac0beb615b7df8eccd7083c8d473c26392
Psydev <blaat2k@mail.com>
2026-08-09 22:30:55 +0200

    add grid positions for icon placement

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,17 +1,35 @@
 <template>
-  <div>
-    <ul class="list-none p-0 grid grid-cols-3">
-      <li v-for="(icon, index) in icons" :key="index">
-        <Icon :icon="icon" :class="playerClass"></Icon>
-      </li>
-    </ul>
-  </div>
+  <ul class="relative w-100 h-100 max-w-full list-none p-0">
+    <li
+      v-for="(icon, index) in icons"
+      :key="index"
+      class="absolute"
+      :style="{
+        left: `${positions[index].x}%`,
+        top: `${positions[index].y}%`,
+        transform: 'translate(-50%, -50%)',
+      }"
+    >
+      <Icon :icon="icon" :class="playerClass" />
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
 import type { IconName } from '@/data/icons'
 import Icon from './Icon.vue'
 import { computed } from 'vue'
+
+const positions = [
+  { x: 50, y: 50 }, // center
+  { x: 32, y: 22 },
+  { x: 70, y: 25 },
+  { x: 18, y: 50 },
+  { x: 82, y: 50 },
+  { x: 30, y: 78 },
+  { x: 70, y: 78 },
+  { x: 50, y: 12 },
+]
 
 const playerClass = computed(() => {
   return type === 'player'
