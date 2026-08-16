81f8dfe1fabd7ab93a5def5d2a0c6dd2fac30da6
Psydev <blaat2k@mail.com>
2026-08-16 18:46:18 +0200

    Add to-home link logic

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,12 +1,26 @@
 <template>
   <nav class="bg-navy py-4">
     <div class="container text-white">
-      <RouterLink to="/" class="text-white text-3xl no-underline">Dobble</RouterLink>
-      match the icon
+      <RouterLink
+        v-if="route.path !== '/'"
+        to="/"
+        title="Go back home"
+        class="text-white text-3xl no-underline decoration-2 underline-offset-3 hover:underline"
+        >Dobble</RouterLink
+      >
+
+      <span v-else class="text-white text-3xl">Dobble</span>
+
+      <span class="mx-2">|</span>
+      <span class="text-sm">match the icon</span>
     </div>
   </nav>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
 
 <style scoped></style>
