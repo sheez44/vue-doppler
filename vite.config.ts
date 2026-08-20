7105103ab875b98f9bf187259d691be57ee67e77
Psydev <blaat2k@mail.com>
2026-08-20 10:15:00 +0200

    remove db.json watch

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
+
+  server: {
+    watch: {
+      ignored: ['**/db.json'],
+    },
+  },
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
